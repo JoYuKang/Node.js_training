b872f312b9b0fad64baef0f103399f8dc9b13027
Yu Knag <ykang9533@naver.com>
2021-02-09 12:12:06 +0900

    #2 노드 기초

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,41 @@
 const express = require('express');
+const { renderString } = require('nunjucks');
 
 const router = express.Router();   
 
-router.get('/',(req,res)=>{
+function testMiddleware1(req,res,next){
+    console.log('첫번째 미들웨어');
+    next();
+}
+
+function testMiddleware2(req,res,next){
+    console.log('두번째 미들웨어');
+    next();
+}
+router.get('/', testMiddleware1, testMiddleware2, (req,res)=>{
     res.send('admin 이후 url');
 });
 
 router.get('/products',(req,res)=>{
-    res.send('admin products');
+    //res.send('admin products');
+    res.render('admin/products.html',{
+        message : 'hello! node render',
+        online : 'express codes'
+    });
 });
 
-module.exports = router;
+router.get('/products/write',(req,res) => {
+    res.render('admin/write.html',{
+        
+    });
+});
+
+// router.post('/product/write',(req,res) => {
+//     res.send(req.body.name);
+// });
+
+router.post('/products/write', ( req , res ) => {
+    res.send(req.body);
+});
+
+module.exports = router;    
